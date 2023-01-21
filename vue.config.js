c195e10cea47fe6374d672bbda08d71fb665da36
Andrey999K <andrey1234xxxx@mail.ru>
2023-01-22 01:20:58 +0300

    for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/elevator-test-task/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/elevator-test-task/'
+    : '/'
 })
